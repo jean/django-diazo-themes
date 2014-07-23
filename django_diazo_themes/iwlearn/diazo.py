bccaa827ff3e35f40d33c2bd288fd5bdfbba43a1
tayas <taya@iwlearn.org>
2014-07-23 19:21:34 +0700

    - skip /admin for theming

--- django_diazo_themes/iwlearn/diazo.py
+++ django_diazo_themes/iwlearn/diazo.py
@@ -7,6 +7,7 @@ from django_diazo.theme import DiazoTheme, registry
 class IwlearnTheme(DiazoTheme):
     name = 'IW:LEARN'
     slug = 'iwlearn'
+    pattern = '^(?!.*admin)'  # Theme everything but /admin
 
 
 registry.register(IwlearnTheme)
